a5c973abe6c7589a381fe347668b69f4fa7e3cf6
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-04-14 20:55:09 +0200

    creation of the first 3 pokemons on the list

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let pokemonList =
+[
+  {
+    name:'Bulbasur',
+    height:0.7,
+    type:['Grass', 'Poison']
+  },
+
+  {
+    name:'Charmander',
+    height:0.6,
+    type:'Fire'
+  },
+  {
+    name:'Squirtle',
+    height:0.5,
+    type:'Water',
+  }
+];
