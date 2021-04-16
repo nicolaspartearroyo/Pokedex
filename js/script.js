a53076a1b54c957b0de930400e8b6a3eb8ab1ee5
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-04-16 17:53:08 +0200

    loop created,conditional height added

--- js/script.js
+++ js/script.js
@@ -3,17 +3,28 @@ let pokemonList =
   {
     name:'Bulbasur',
     height:0.7,
-    type:['Grass', 'Poison']
+    type:'Grass'
   },
 
   {
     name:'Charmander',
-    height:0.6,
+    height:1.6,
     type:'Fire'
   },
+
   {
     name:'Squirtle',
     height:0.5,
-    type:'Water',
+    type:'Water'
   }
 ];
+
+for (let i = 0; i < pokemonList.lenght; i++); {
+  document.write(pokemonList[i].name + '. Height: ' + pokemonList[i].height + '. Type: ' + pokemonList[i].type);
+}
+
+for (let i = 0; i < pokemonList.lenght; i++); {
+  if (pokemonList[i].height > 1) {
+    document.write('. Woow, thats big!');
+  }
+}
