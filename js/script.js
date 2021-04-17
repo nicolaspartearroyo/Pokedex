6cc520c90a5e3d70842666a9227d6597ef1feace
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-04-17 02:23:14 +0200

    length and ; error corrected. syntax corrected

--- js/script.js
+++ js/script.js
@@ -19,26 +19,13 @@ let pokemonList =
   }
 ];
 
+
 // Write Pokemons name + height + type.
 // For some reason I have to put var i = number otherwise console tell me variable is undefined.
-var i = 0;
-for (let i = 0; i < pokemonList.lenght; i++); {
-  document.write(pokemonList[i].name + '. Height: ' + pokemonList[i].height + '. Type: ' + pokemonList[i].type);
-}
-
-var i = 1;
-for (let i = 0; i < pokemonList.lenght; i++); {
-  document.write(pokemonList[i].name + '. Height: ' + pokemonList[i].height + '. Type: ' + pokemonList[i].type);
-}
-
-var i = 2;
-for (let i = 0; i < pokemonList.lenght; i++); {
-  document.write(pokemonList[i].name + '. Height: ' + pokemonList[i].height + '. Type: ' + pokemonList[i].type);
-}
-
-// Height condition
-for (let i = 0; i < pokemonList.lenght; i++); {
-  if (pokemonList[i].height > 1) {
-    document.write('. Woow, thats big!');
+    for (let i = 0; i < pokemonList.length; i++) {
+      document.write(pokemonList[i].name + '. Height: ' + pokemonList[i].height + '. Type: ' + pokemonList[i].type + `<br>`)
+      if (pokemonList[i].height > 1) {
+      document.write('. Woow, thats big!');
+      document.write(`<br>`)
+    }
   }
-}
