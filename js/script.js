4d9749382ef9b622b1201618514ffc7435037b90
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-04-19 21:25:10 +0200

    updated simplicity on the code

--- js/script.js
+++ js/script.js
@@ -21,11 +21,14 @@ let pokemonList =
 
 
 // Write Pokemons name + height + type.
-// For some reason I have to put var i = number otherwise console tell me variable is undefined.
-    for (let i = 0; i < pokemonList.length; i++) {
-      document.write(pokemonList[i].name + '. Height: ' + pokemonList[i].height + '. Type: ' + pokemonList[i].type + `<br>`)
-      if (pokemonList[i].height > 1) {
-      document.write('. Woow, thats big!');
-      document.write(`<br>`)
+
+function printArrayList(list) {
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write("<p>" + pokemonList[i].name + '. Height: ' + pokemonList[i].height + '. Type: ' + pokemonList[i].type + "</p>")
+    if (pokemonList[i].height > 1) {
+    document.write('. Woow, thats big!');
     }
   }
+}
+
+printArrayList(pokemonList);
